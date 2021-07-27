cfe1799849ff963c300519b16568b8e7ab14539a
slhshamloo <slhhero@gmail.com>
2021-07-27 15:34:20 +0430

    added croping for boundaries

--- slhfdtd/visulization.py
+++ slhfdtd/visulization.py
@@ -1,18 +1,32 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+from .boundaries import AutoPML
+
 
 class Visualizer():
     def __init__(self, solver):
         self.solver = solver
     
-    def plot1d_field(self, artist, field, axis_space=0, axis_field=2,
-                 slice_first_coordinate=0, slice_second_coordinate=0,
-                 begin_space=0, end_space=None):
-        axis_length = self.solver.length[axis_space]
-        
+    def plot1d_field(self, ax, field, axis_space=0, axis_field=2,
+                     slice_first_coordinate=0, slice_second_coordinate=0,
+                     begin_space=None, end_space=None,
+                     crop_boundaries=True, color='blue'):
+        if begin_space is None:
+            begin_space = 0
         if end_space is None:
-            end_space = axis_length
+            end_space = self.solver.length[axis_space]
+        
+        if crop_boundaries:
+            for boundary in self.solver.boundaries:
+                if isinstance(boundary, AutoPML):
+                    begin_crop = boundary.begin_bound[axis_space]
+                    end_crop = boundary.end_bound[axis_space]
+                    if begin_space < begin_crop:
+                        begin_space = begin_crop
+                    if end_space > end_crop:  
+                        end_space = end_crop
+                    break
         
         begin_cell = round(begin_space / self.solver.grid_dist)
         end_cell = round(end_space / self.solver.grid_dist)
@@ -30,28 +44,53 @@ class Visualizer():
                 begin_cell:end_cell, axis_field]
         
         data_space = np.linspace(begin_space, end_space, end_cell - begin_cell)
-        artist.set_data(data_space, data_field)
+        ax.plot(data_space, data_field, c=color)
+        ax.relim()
     
-    def plot1d_E(self, artist, axis_space=0, axis_E=2,
+    def plot1d_E(self, ax, axis_space=0, axis_E=2,
                  slice_first_coordinate=0, slice_second_coordinate=0,
-                 begin_space=0, end_space=None):
-        self.plot1d_field(artist, self.solver.E, axis_space, axis_E,
+                 begin_space=None, end_space=None,
+                 crop_boundaries=True, color='blue'):
+        self.plot1d_field(ax, self.solver.E, axis_space, axis_E,
                           slice_first_coordinate, slice_second_coordinate,
-                          begin_space, end_space)
+                          begin_space, end_space, crop_boundaries, color)
     
-    def plot1d_H(self, artist, axis_space=0, axis_H=1,
+    def plot1d_H(self, ax, axis_space=0, axis_H=1,
                  slice_first_coordinate=0, slice_second_coordinate=0,
-                 begin_space=0, end_space=None):
-        self.plot1d_field(artist, self.solver.H, axis_space, axis_H,
+                 begin_space=None, end_space=None,
+                 crop_boundaries=True, color='red'):
+        self.plot1d_field(ax, self.solver.H, axis_space, axis_H,
                           slice_first_coordinate, slice_second_coordinate,
-                          begin_space, end_space)
+                          begin_space, end_space, crop_boundaries, color)
     
     def plot2d_field(self, ax, field, color_map, slice_z=0,
-                     begin_x=0, begin_y=0, end_x=None, end_y=None):
+                     begin_x=None, begin_y=None, end_x=None, end_y=None,
+                     crop_boundaries=True):
+        if begin_x is None:
+            begin_x = 0
         if end_x is None:
             end_x = self.solver.length[0]
+        if begin_y is None:
+            begin_y = 0
         if end_y is None:
-            end_y = self.solver.length[0]
+            end_y = self.solver.length[1]
+
+        if crop_boundaries:
+            for boundary in self.solver.boundaries:
+                if isinstance(boundary, AutoPML):
+                    begin_crop_x = boundary.begin_bound[0]
+                    end_crop_x = boundary.end_bound[0]
+                    begin_crop_y = boundary.begin_bound[1]
+                    end_crop_y = boundary.end_bound[1]
+                    if begin_x < begin_crop_x:
+                        begin_x = begin_crop_x
+                    if begin_y < begin_crop_y:
+                        begin_y = begin_crop_y
+                    if end_x > end_crop_x:
+                        end_x = end_crop_x
+                    if end_y > end_crop_y:
+                        end_y = end_crop_y
+                    break
         
         begin_x_cell = round(begin_x / self.solver.grid_dist)
         begin_y_cell = round(begin_y / self.solver.grid_dist)
@@ -63,14 +102,18 @@ class Visualizer():
                                begin_y_cell:end_y_cell,
                                slice_z_cell, :]**2,
                             axis=2)**0.5
-        ax.imshow(data_field, cmap=color_map)
+        ax.imshow(data_field.T, cmap=color_map)
+        ax.relim()
+        ax.autoscale_view()
     
-    def plot2d_E(self, ax, color_map='Blues', slice_z=0, begin_x=0, begin_y=0,
-                 end_x=None, end_y=None):
+    def plot2d_E(self, ax, color_map='Blues', slice_z=0,
+                 begin_x=None, begin_y=None, end_x=None, end_y=None,
+                 crop_boundaries=True):
         self.plot2d_field(ax, self.solver.E, color_map, slice_z,
-                          begin_x, begin_y, end_x, end_y)
+                          begin_x, begin_y, end_x, end_y, crop_boundaries)
     
-    def plot2d_H(self, ax, color_map='Reds', slice_z=0, begin_x=0, begin_y=0,
-                 end_x=None, end_y=None):
+    def plot2d_H(self, ax, color_map='Reds', slice_z=0,
+                 begin_x=None, begin_y=None, end_x=None, end_y=None,
+                 crop_boundaries=True):
         self.plot2d_field(ax, self.solver.H, color_map, slice_z,
-                          begin_x, begin_y, end_x, end_y)
+                          begin_x, begin_y, end_x, end_y, crop_boundaries)
