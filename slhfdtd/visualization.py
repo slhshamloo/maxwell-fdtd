5c4acf379e330b2706bc7b1c5978f3fea22f1b31
slhshamloo <slhhero@gmail.com>
2021-08-01 16:11:41 +0430

    added functions for drawing objects

--- slhfdtd/visualization.py
+++ slhfdtd/visualization.py
@@ -12,6 +12,56 @@ class Visualizer():
     def __init__(self, solver):
         self.solver = solver
 
+    def plot1d_E(self, ax, axis_space=0, axis_E=2,
+                 slice_first_coordinate=0, slice_second_coordinate=0,
+                 begin_space=None, end_space=None, crop_boundaries=True,
+                 color='blue', obj_color='lightskyblue'):
+        self.plot1d_field(ax, self.solver.E, axis_space, axis_E,
+                          slice_first_coordinate, slice_second_coordinate,
+                          begin_space, end_space, crop_boundaries,
+                          color, obj_color)
+
+    def plot1d_H(self, ax, axis_space=0, axis_H=1,
+                 slice_first_coordinate=0, slice_second_coordinate=0,
+                 begin_space=None, end_space=None, crop_boundaries=True,
+                 color='red', obj_color='lightskyblue'):
+        self.plot1d_field(ax, self.solver.H, axis_space, axis_H,
+                          slice_first_coordinate, slice_second_coordinate,
+                          begin_space, end_space, crop_boundaries,
+                          color, obj_color)
+
+    def plot2d_E(self, ax, slice_z=0,
+                 begin_x=None, begin_y=None, end_x=None, end_y=None,
+                 crop_boundaries=True, norm='lin', cmap='Blues',
+                 obj_color='lime'):
+        self.plot2d_field(ax, self.solver.E, slice_z,
+                          begin_x, begin_y, end_x, end_y,
+                          crop_boundaries, norm, cmap, obj_color)
+
+    def plot2d_H(self, ax, slice_z=0,
+                 begin_x=None, begin_y=None, end_x=None, end_y=None,
+                 crop_boundaries=True, norm='lin', cmap='Reds',
+                 obj_color='lime'):
+        self.plot2d_field(ax, self.solver.H, slice_z,
+                          begin_x, begin_y, end_x, end_y,
+                          crop_boundaries, norm, cmap, obj_color)
+    
+    def draw_object_1d(self, ax, *objects, axis_space=0, color='lime'):
+        for obj in objects:
+            ax.axvspan(obj.begin_pos[axis_space],
+                       obj.end_pos[axis_space],
+                       lw=0, alpha=0.5, color=color)
+    
+    def draw_object_2d(self, ax, *objects, color='lime'):
+        for obj in objects:
+            if isinstance(obj, Slab):
+                ax.add_patch(patches.Rectangle(
+                    (obj.begin_pos[0], obj.begin_pos[1]),
+                    obj.end_pos[0] - obj.begin_pos[0],
+                    obj.end_pos[1] - obj.begin_pos[1],
+                    lw=0, alpha=0.25, color=color
+                ))
+    
     def plot1d_field(self, ax, field, axis_space=0, axis_field=2,
                      slice_first_coordinate=0, slice_second_coordinate=0,
                      begin_space=None, end_space=None, crop_boundaries=True,
@@ -36,7 +86,7 @@ class Visualizer():
         end_cell = round(end_space / self.solver.grid_dist)
         slice_first_cell = round(slice_first_coordinate
                                  / self.solver.grid_dist)
-        slice_second_cell = round(slice_first_coordinate
+        slice_second_cell = round(slice_second_coordinate
                                   / self.solver.grid_dist)
 
         if axis_space == 0:
@@ -54,30 +104,12 @@ class Visualizer():
         ax.plot(data_space, data_field, c=color)
 
         if obj_color is not None:
-            for obj in self.solver.objects:
-                ax.axvspan(obj.begin_pos[axis_space], obj.end_pos[axis_space],
-                        lw=0, alpha=0.5, color=obj_color)
+            self.draw_object_1d(ax, *self.solver.objects,
+                                axis_space=axis_space,
+                                color=obj_color)
         
         ax.relim()
-
-    def plot1d_E(self, ax, axis_space=0, axis_E=2,
-                 slice_first_coordinate=0, slice_second_coordinate=0,
-                 begin_space=None, end_space=None, crop_boundaries=True,
-                 color='blue', obj_color='lightskyblue'):
-        self.plot1d_field(ax, self.solver.E, axis_space, axis_E,
-                          slice_first_coordinate, slice_second_coordinate,
-                          begin_space, end_space, crop_boundaries,
-                          color, obj_color)
-
-    def plot1d_H(self, ax, axis_space=0, axis_H=1,
-                 slice_first_coordinate=0, slice_second_coordinate=0,
-                 begin_space=None, end_space=None, crop_boundaries=True,
-                 color='red', obj_color='lightskyblue'):
-        self.plot1d_field(ax, self.solver.H, axis_space, axis_H,
-                          slice_first_coordinate, slice_second_coordinate,
-                          begin_space, end_space, crop_boundaries,
-                          color, obj_color)
-
+    
     def plot2d_field(self, ax, field, slice_z=0,
                      begin_x=None, begin_y=None, end_x=None, end_y=None,
                      crop_boundaries=True, norm='lin', cmap='jet',
@@ -131,32 +163,9 @@ class Visualizer():
         plt.colorbar(pcm, ax=ax)
 
         if obj_color is not None:
-            for obj in self.solver.objects:
-                if isinstance(obj, Slab):
-                    ax.add_patch(patches.Rectangle(
-                        (obj.begin_pos[0], obj.begin_pos[1]),
-                        obj.end_pos[0] - obj.begin_pos[0],
-                        obj.end_pos[1] - obj.begin_pos[1],
-                        lw=0, alpha=0.25,
-                        color=obj_color
-                    ))
+            self.draw_object_2d(ax, *self.solver.objects,
+                                color=obj_color)
 
         ax.relim()
         ax.autoscale_view()
         ax.set_aspect('auto')
-
-    def plot2d_E(self, ax, slice_z=0,
-                 begin_x=None, begin_y=None, end_x=None, end_y=None,
-                 crop_boundaries=True, norm='lin', cmap='Blues',
-                 obj_color='lime'):
-        self.plot2d_field(ax, self.solver.E, slice_z,
-                          begin_x, begin_y, end_x, end_y,
-                          crop_boundaries, norm, cmap, obj_color)
-
-    def plot2d_H(self, ax, slice_z=0,
-                 begin_x=None, begin_y=None, end_x=None, end_y=None,
-                 crop_boundaries=True, norm='lin', cmap='Reds',
-                 obj_color='lime'):
-        self.plot2d_field(ax, self.solver.H, slice_z,
-                          begin_x, begin_y, end_x, end_y,
-                          crop_boundaries, norm, cmap, obj_color)
