8ac94b3a5432eb59f840c9c892076fddb8ff7455
slhshamloo <slhhero@gmail.com>
2021-08-06 11:15:43 +0430

    made proper package init file

---
+++ slhfdtd/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Saleh Shamloo Ahmadi"
+
+from .solving import Solver
+from .sources import Source, PointSource, LineSource, pulse
+from .objects import Slab
+from .boundaries import PML, AutoPML, Exact1DAbsorber
+from .visualization import Visualizer, draw_object_1d, draw_object_2d
