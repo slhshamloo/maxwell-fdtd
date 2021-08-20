939200c9f0f5227b2157516dc2fae677d2ae0eab
slhshamloo <slhhero@gmail.com>
2021-08-20 23:56:53 +0430

    added new features to init file

--- slhfdtd/__init__.py
+++ slhfdtd/__init__.py
@@ -2,6 +2,6 @@ __author__ = "Saleh Shamloo Ahmadi"
 
 from .solving import Solver
 from .sources import Source, PointSource, LineSource, square_wave
-from .objects import Slab
+from .objects import Slab, Ball
 from .boundaries import Reflector, AutoReflector, PML, AutoPML, Exact1DAbsorber
 from .visualization import Visualizer, draw_object_1d, draw_object_2d
